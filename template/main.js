a22ad5de95a5665e8ae11a08d85839867037c77b
Henrique Santos <henriquebd2000.hs@gmail.com>
2022-10-14 07:50:50 -0300

    update files

--- template/main.js
+++ template/main.js
@@ -6,17 +6,18 @@
 // } from './api.js'
 
 const API_KEY = 'api_key=4a8b501b2a2c0caf295cbf61dacbcaa0'
-const BASE_URL = 'https://api.themoviedb.org/3/movie/550?api_key=4a8b501b2a2c0caf295cbf61dacbcaa0'
+const BASE_URL = 'https://api.themoviedb.org/3/movie/'
 const IMG_URL = 'https://image.tmdb.org/t/p/w500'
 const language = 'language=pt-BR'
+const id = (num) => Math.floor(Math.random() * num)
+
 
 function getFilm() {
-  // updateFilm()
 
-  fetch(BASE_URL)
+  fetch(`${BASE_URL}${id(999999)}?${API_KEY}`)
     .then(response => response.json())
     .then(data => {
-      // film_poster.src = data.poster_path
+      film_poster.src = `${IMG_URL}${data.poster_path}`
       film_title.textContent = data.original_title
       film_description.textContent = data.overview
     })
@@ -24,7 +25,7 @@ function getFilm() {
 }
 
 getFilm()
-// updateFilm()
+
 
 // function updateFilm() {
 //   fetch(BASE_URL, {
@@ -34,6 +35,7 @@ getFilm()
 //       'Content-type': 'application/json; charset=UTF-8'
 //     }
 //   })
+//     .then(response => response.json())
 //     .catch(error => console.error(error))
 // }
 
