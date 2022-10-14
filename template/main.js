e305ae60bb627847cc6afac628ffa99a54ca8357
Henrique Santos <henriquebd2000.hs@gmail.com>
2022-10-14 11:21:08 -0300

    update files

--- template/main.js
+++ template/main.js
@@ -10,36 +10,28 @@ const BASE_URL = 'https://api.themoviedb.org/3/movie/'
 const IMG_URL = 'https://image.tmdb.org/t/p/w500'
 const language = 'language=pt-BR'
 const id = (num) => Math.floor(Math.random() * num)
+const visibleFilm = document.querySelector('.movies')
+const movieNotFound = document.querySelector('.movie_not_found')
 
+function getMovie() {
 
-function getFilm() {
+  visibleFilm.removeAttribute('style')
 
   fetch(`${BASE_URL}${id(999999)}?${API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-      film_poster.src = `${IMG_URL}${data.poster_path}`
-      film_title.textContent = data.original_title
-      film_description.textContent = data.overview
+    .then(response => {
+      if(response.status === 404){
+        movieNotFound.removeAttribute('style')
+
+      } else {response.json()}
     })
-    .catch(error => console.error(error))
+    .then(data => {
+      movie_poster.src = `${IMG_URL}${data.poster_path}`
+      movie_title.textContent = data.title
+      movie_description.textContent = data.overview
+    })
+    .catch(error => {
+      console.error(error)
+    })
 }
 
-getFilm()
 
-
-// function updateFilm() {
-//   fetch(BASE_URL, {
-//     method: 'PUT',
-//     body: JSON.stringify(updatedFilm), 
-//     headers: {
-//       'Content-type': 'application/json; charset=UTF-8'
-//     }
-//   })
-//     .then(response => response.json())
-//     .catch(error => console.error(error))
-// }
-
-// const updatedFilm = {
-//   // poster_path: `${IMG_URL}${poster_path}`,
-//   original_language: `${language}`
-// }
