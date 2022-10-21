f5c9bca0f5da35f15705c1461fdc8754aa0e7988
Henrique Santos <henriquebd2000.hs@gmail.com>
2022-10-21 02:55:02 -0300

    add and upgrade files

--- template/api.js
+++ template/api.js
@@ -5,23 +5,5 @@ export const BASE_URL = 'https://api.themoviedb.org/3/movie/550?api_key=4a8b501b
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500'
 export const language = 'language=pt-BR'
 
-function getMovie() {
 
-  visibleFilm.removeAttribute('style')
-
-  fetch(`${BASE_URL}${id(999999)}?${API_KEY}`)
-    .then(response => {
-      if(response.status === 404){
-        movieNotFound.removeAttribute('style')
-
-      } else {response.json()}
-    })
-    .then(data => {
-      movie_poster.src = `${IMG_URL}${data.poster_path}`
-      movie_title.textContent = data.title
-      movie_description.textContent = data.overview
-
-    })
-    .catch(error => console.error(error))
-}
 
--- template/main.js
+++ template/main.js
@@ -1,5 +1,5 @@
 // import {
-//   API_KEY, 
+//   API_KEY,
 //   BASE_URL,
 //   IMG_URL,
 //   language,
@@ -9,29 +9,37 @@ const API_KEY = 'api_key=4a8b501b2a2c0caf295cbf61dacbcaa0'
 const BASE_URL = 'https://api.themoviedb.org/3/movie/'
 const IMG_URL = 'https://image.tmdb.org/t/p/w500'
 const language = 'language=pt-BR'
-const id = (num) => Math.floor(Math.random() * num)
-const visibleFilm = document.querySelector('.movies')
-const movieNotFound = document.querySelector('.movie_not_found')
+const visibleFilm = document.getElementById('movies')
+const movieNotFound = document.getElementById('movie_not_found')
+
+const id = num => Math.floor(Math.random() * num)
+
 
 function getMovie() {
 
-  visibleFilm.removeAttribute('style')
-
-  fetch(`${BASE_URL}${id(999999)}?${API_KEY}`)
+  fetch(`${BASE_URL}${id(100)}?${API_KEY}&${language}`)
     .then(response => {
-      if(response.status === 404){
-        movieNotFound.removeAttribute('style')
-
-      } else {response.json()}
+      if (response.status === 404) {
+        movieNotFound.classList.remove('display-none')
+        movieNotFound.classList.add('movie_not_found')
+        visibleFilm.classList.add('display-none')
+        visibleFilm.classList.remove('movies')
+      } else {
+        movieNotFound.classList.add('display-none')
+        movieNotFound.classList.remove('movie_not_found')
+        visibleFilm.classList.remove('display-none')
+        visibleFilm.classList.add('movies')
+      }
+      return response.json()
+      
     })
     .then(data => {
       movie_poster.src = `${IMG_URL}${data.poster_path}`
       movie_title.textContent = data.title
       movie_description.textContent = data.overview
+
     })
     .catch(error => {
       console.error(error)
     })
 }
-
-
